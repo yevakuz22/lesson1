5b8883f8d4702341b1e602e02d8791099a6a89d0
yevakuz22 <yevakuzminchuk@gmail.com>
2024-04-25 20:58:07 +0300

    added file main.py

---
+++ дз2.py
@@ -0,0 +1,53 @@
+class Rectangle:
+
+    def __init__(self, width=7, height=11):
+        self.width = width
+        self.height = height
+
+        def __len__(self):
+            return self.width != 7
+
+        def __del__(self):
+            print("calculate area and calculate perimeter")
+
+        def __bool__(self):
+            return self.height != 11
+
+Rectangle = width (7)
+Rectangle = height (11)
+print(rectangle.height)
+print(rectangle.width)
+print(len())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
